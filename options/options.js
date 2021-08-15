acd76f12c78baa0639ee2adc534b7b91b3f2954b
Crowdbotics <team@crowdbotics.com>
2021-08-15 23:15:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "restless_waterfall_29837Identifier",
+  url: "https://restless_waterfall_29837Identifier.botics.co",
+  api: "https://restless_waterfall_29837Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
